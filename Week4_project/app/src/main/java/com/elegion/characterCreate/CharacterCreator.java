9a5d614788b649440e99bcc98058c122be121168
kanootoko <kanootoko@gmail.com>
2019-06-27 21:32:30 +0300

    Week 4 project is to be done soon

---
+++ Week4_project/app/src/main/java/com/elegion/characterCreate/CharacterCreator.java
@@ -0,0 +1,160 @@
+package com.elegion.characterCreate;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Observable;
+
+public class CharacterCreator extends Observable  implements Serializable{
+
+    public enum Specialization {
+        WARRIOR, ARCHER, MAGE
+    }
+
+    public enum Race {
+        HUMAN, ELF, ORC, DWARF
+    }
+
+    public enum Attribute {
+        STRENGTH, AGILITY, INTELLECT, STAMINA, LUCK
+    }
+
+    public enum Perk {
+        BERSERK, CALM, LIGHTWEIGHT, HEAVYARMORED, OBSERVANT, MEDITATIONS
+    }
+
+    private String mName;
+    private Specialization mSpecialization;
+    private Race mRace;
+    private int mAvailablePoints;
+
+    private Map<String, Integer> mAttributesMap = new HashMap<>();
+    private Map<String, Boolean> mPerksMap = new HashMap<>();
+
+
+    public CharacterCreator() {
+        mRace = Race.HUMAN;
+        mSpecialization = Specialization.WARRIOR;
+        mAvailablePoints = 5;
+        mAttributesMap.put(Attribute.STRENGTH.name(), 5);
+        mAttributesMap.put(Attribute.AGILITY.name(), 5);
+        mAttributesMap.put(Attribute.INTELLECT.name(), 5);
+        mAttributesMap.put(Attribute.STAMINA.name(), 5);
+        mAttributesMap.put(Attribute.LUCK.name(), 5);
+    }
+
+    private static String[] getNamesFromEnum(Class<? extends Enum> in) {
+        String[] res = new String[in.getEnumConstants().length];
+        int i = 0;
+        for (Enum a : in.getEnumConstants()) {
+            String tmp = a.toString();
+            res[i++] = tmp.substring(0, 1).toUpperCase() + tmp.substring(1).toLowerCase();
+        }
+        return res;
+    }
+
+    public String[] getSpecializations() {
+        return CharacterCreator.getNamesFromEnum(Specialization.class);
+
+    }
+
+
+    public void setSpecialization(int position) {
+        Specialization[] specs = Specialization.values();
+        if (position < 0)
+            position = 0;
+        else if (position > specs.length)
+            position = specs.length - 1;
+        mSpecialization = specs[position];
+
+    }
+
+    public String[] getRaces() {
+        return CharacterCreator.getNamesFromEnum(Race.class);
+    }
+
+    public void setRace(int position) {
+        Race[] races = Race.values();
+        if (position < 0)
+            position = 0;
+        else if (position > races.length)
+            position = races.length - 1;
+        mRace = races[position];
+    }
+
+    public String[] getAttributes() {
+        return CharacterCreator.getNamesFromEnum(Attribute.class);
+
+    }
+
+    public String[] getPerks() {
+        return CharacterCreator.getNamesFromEnum(Perk.class);
+
+    }
+    public void updateAttributeValue(int position, int updateTo) {
+        Attribute[] attrs = Attribute.values();
+        if (position < 0)
+            position = 0;
+        else if (position > attrs.length)
+            position = attrs.length - 1;
+        int value = mAttributesMap.get(attrs[position].name());
+        if ((mAvailablePoints >= updateTo) && (value + updateTo >= 0)){
+            value += updateTo;
+            mAvailablePoints -= updateTo;
+            mAttributesMap.put(attrs[position].name(), value);
+        }
+        setChanged();
+        notifyObservers();
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getAvailablePoints() {
+        return String.valueOf(mAvailablePoints);
+    }
+
+    public Map<String, Integer> getAttributesMap() {
+        return mAttributesMap;
+    }
+
+    public void checkPerk(String text, boolean isChecked) {
+        mPerksMap.put(text, isChecked);
+    }
+
+    public Character create() {
+        Character character = new Character();
+        character.setName(mName);
+        character.setRace(mRace);
+        character.setSpecialization(mSpecialization);
+        character.setAttributes(mAttributesMap);
+        character.setPerks(mPerksMap);
+        character.calculateParameters();
+        return character;
+    }
+
+    public Specialization getSpecialization() {
+        return mSpecialization;
+    }
+
+    public Race getRace() {
+        return mRace;
+    }
+
+    public Map<String, Boolean> getPerksMap() {
+        return mPerksMap;
+    }
+
+    public void setAvailablePoints(int availablePoints) {
+        mAvailablePoints = availablePoints;
+    }
+
+    public int getRacePosition() {
+        return mRace.ordinal();
+    }
+
+    public int getSpecializationPosition() {
+        return mSpecialization.ordinal();
+    }
+}
